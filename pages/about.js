d9f35c9b13598f76971a2e345cadaf448541be73
kichanyoon-alticast <pro4697@naver.com>
2022-08-30 17:58:26 +0900

    Day 1: ~#1.3

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar';
+
+export function About() {
+	return (
+		<div>
+			<NavBar />
+			about
+		</div>
+	);
+}
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import NavBar from '../components/NavBar';
+
+export function Home() {
+	return (
+		<div>
+			<NavBar />
+			home
+		</div>
+	);
+}
+
+export default Home;
